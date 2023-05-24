67750875bb5704e8d23457fa8faf9383ef0cb319
bluecodemx <hizashi27@gmail.com>
2023-05-24 16:51:36 +0700

    add astra

--- Astra_structure.php
+++ Astra_structure.php
@@ -17,14 +17,14 @@ echo (nUmber),("\n");
 echo $lIne ,("\n");
 //Finish head Script
 
-//Variable Start Script Bio data cashier
+//Start Script Bio data cashier
 $bio_cashier =[
     "no_account" => 2270,
     "cashier" => [
         "pgw_01" => [
             "id" => 224,
             "Name" => "Michael",
-             "born" => "Jakarta",
+             "Shift_Time" => "Night",
         ],
         "pgw_02" => [
             "id" => 225,
@@ -47,3 +47,32 @@ $bio_cashier =[
 echo "\t","No_Account: " ;
 echo ($bio_cashier["no_account"]) , "\n";
 echo $lIne , "\n" ;
+//start bio cashier Script array echo
+$id=$bio_cashier["cashier"]["pgw_01"]["id"];
+$pgw_01 =$bio_cashier["cashier"]["pgw_01"]["Name"];
+$Shift_Time =$bio_cashier["cashier"]["pgw_01"]["Shift_Time"];
+echo $id, "\t";
+echo $pgw_01, "\t","\t";
+echo $Shift_Time, "\n", "\n";
+
+//List Item Buyer
+//Start Script list item client
+$list_item = [
+    "frst_item" => ["Knalpot","2 set","type2204"],
+    "scnd_item" => ["Oli Yamaha",1,"type44056"]
+];
+// Memanggil elemen-elemen array
+$firstItem = $list_item["frst_item"];
+$secondItem = $list_item["scnd_item"];
+
+// Memanggil nilai dari elemen-elemen array
+$nameFirstItem = $list_item["frst_item"][0];
+$quantityFirstItem = $list_item["frst_item"][1];
+
+
+// Menampilkan nilai dari elemen-elemen array
+echo $nameFirstItem,"\t"; // Output: Knalpot
+echo $quantityFirstItem;//output quntity frst_item
+
+
+
