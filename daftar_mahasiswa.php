c93208beb6ad92bf2771ea614f9f167ef35d5efc
bluecodemx <hizashi27@gmail.com>
2023-05-31 16:57:17 +0700

    daftar

--- daftar_mahasiswa.php
+++ daftar_mahasiswa.php
@@ -1,13 +1,28 @@
 <?php
-// start Script Matkul
+// start Declaration Head Script Matkul
 define ("kampus", "STMIK Muhammadiyah Jakarta");
 $Semester= ["1","2","3","4","5"];
 $matkul=[ "Filsafat","Pancasila","Matematika" ];
 $line ="-----------------------------------";
-// echo Daftar Nilai Kampus
+// finish Declaration Head Script Matkul
+// start Daftar Nilai Matkul
+
+$total=0;
+$daily_Point = 100;
+$absensi_Point = 70;
+
+$Total= $daily_Point+$absensi_Point;
+$result=$Total /(2) ;
+
+// finish Daftar Nilai Kampus
+// echo Head Script Matkul
 echo kampus,"\n";
 echo "Mata Kuliah: ";
 echo ($matkul[0]),"\n";
+echo $line, "\n";
 echo "Semester: ";
 echo ($Semester[0]),"\n";
-echo $line;
+echo $line,"\n";
+// 
+echo $result;
+
