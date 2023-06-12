0698721cb16a0f2aa3876de46a1e1ded120c6722
bluecodemx <hizashi27@gmail.com>
2023-06-12 17:03:51 +0700

    revisi

--- data_barang.php
+++ data_barang.php
@@ -2,12 +2,9 @@
 // START FUNCTION STOCK BARANG
 function stockNumber (int $noBarang){
       for ($nobarang=1; $nobarang<=10; $nobarang++):
-     echo "$nobarang.". PHP_EOL;
+            echo "$nobarang.". PHP_EOL;
       endfor;
 
 }
 
-//START FUNCTION INDETITAS PRODUK
-function productIdentity (string $namaBarang, string $expireDate){
-
-}
+stockNumber(4);
