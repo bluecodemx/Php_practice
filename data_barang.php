dfc7fcecd90f2a000888af6b6c0be8da20a2cd6c
bluecodemx <hizashi27@gmail.com>
2023-06-14 01:41:28 +0700

    6/14/2023

--- data_barang.php
+++ data_barang.php
@@ -1,13 +1,22 @@
 <?php
 // START FUNCTION STOCK BARANG
-function stockNumber (int $noBarang){
+function productIdentity (string $noBarang){
+
+// NO BARANG
       for ($nobarang=1; $nobarang<=10; $nobarang++):
-     echo "$nobarang.". PHP_EOL;
+            $namaBarang = $noBarang;
+            $namaBarang = ["sabun", "Shampo"];
+      for ($namaBarang=1; $nobarang <=10; $noBarang ++)
+      
+     echo "$nobarang". PHP_EOL;
       endfor;
 
+// NAMA BARANG
+      $namaBarang = $noBarang;
+      $namaBarang = [
+            "Kecap Manis", "Kecap Asin", "Sambel Terasi"
+      ];
+// CODE EXECUTION
+ return $namaBarang;
 }
-
-//START FUNCTION INDETITAS PRODUK
-function productIdentity (string $namaBarang, string $expireDate){
-
-}
+productIdentity(1);
